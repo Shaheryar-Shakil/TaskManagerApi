8a2f74cdb0270650d6b412817f9b6d206ed892d3
UsamaNadeemKhan <usamanadeem767@gmail.com>
2018-05-09 10:57:20 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,75 @@
+var mysql = require('mysql');
+var express = require('express');
+var app = express();
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "SampleDB"
+});
+
+con.connect(function(err) {
+    if (err) {
+        console.log("Error\n" +  err);
+    }
+    else{
+        console.log("Connected!");
+    }
+});
+
+app.get('/', function(req, res){
+    con.query("SELECT * FROM SampleTable", function(err, rows, fields) {
+        if(!!err) {
+            console.log('Error in query');
+        } else {
+            console.log('Successful query');
+        }
+    });
+});
+
+app.listen(1337);
+
+/*
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+*/
